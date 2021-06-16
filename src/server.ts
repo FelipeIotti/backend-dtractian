62c3059f77b1f08917bde99e04f3233770f56aa8
FelipeIotti <felipeizago@hotmail.com>
2021-06-16 13:14:21 -0300

    finalizado de vdd

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import 'reflect-metadata';
-import express,{Request,Response,NextFunction} from 'express';
+import express from 'express';
 import cors from 'cors';
-import {AppError} from './shared/errors/AppError'
 import './database';
 import './shared/container';
 
@@ -15,19 +14,6 @@ app.use(express.json());
 
 app.use(router);
 
-app.use((err:Error,request:Request,response:Response,next:NextFunction)=>{
-  if(err instanceof AppError){
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-  console.error(err);
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  })
-});
 
 //app.get('/',(request,response) => {return response.json({message:'Hello word!'})});
 
